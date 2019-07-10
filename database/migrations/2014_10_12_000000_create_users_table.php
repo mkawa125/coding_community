c80daf9c655e0017fecf33c2d1a9afcaff073a87
dahabu saidi <dahabusaidi@gmail.com>
2019-07-10 15:35:57 +0300

    add users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,15 @@ class CreateUsersTable extends Migration
             $table->uuid('id');
             $table->primary('id');
             $table->string('name');
+            $table->string('first_name');
+            $table->string('surname');
+            $table->string('username');
+            $table->string('location');
+            $table->string('gender');
+            $table->string('occupation')->nullable();
+            $table->string('skills')->nullable();
+            $table->boolean('status')->default(0);
+            $table->boolean('phone_number');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
