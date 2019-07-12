f59cf5e5b83bf8f451f7aef17b26fda950e39c11
dahabu saidi <dahabusaidi@gmail.com>
2019-07-12 20:04:01 +0300

    add categories migration

---
+++ database/migrations/2019_07_12_194301_create_categories_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->primary('id');
+            $table->string('category_name');
+            $table->text('description');
+            $table->uuid('added_by')->index();
+            $table->softDeletes();
+            $table->timestamps();
+
+            //defining foreign keys
+            $table->foreign('added_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
