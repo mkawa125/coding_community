4da149ad53330f440123c71a654e93f128830c57
dahabu saidi <dahabusaidi@gmail.com>
2019-07-12 21:11:20 +0300

    add categories index page

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -9,5 +9,72 @@
 @extends('layouts.app')
 
 @section('content')
-    assda
-    @endsection
+    <div class="col-md-11" style="margin: 0 auto">
+        <div class="card mb-3" style="padding: 15px">
+            <div class="card-header-tab card-header" style=" border-bottom: 1px solid cadetblue">
+                <div class="card-header-title font-size-lg text-capitalize font-weight-normal">
+                    <i class="header-icon fa fa-list mr-3 text-muted opacity-6" style="font-size: small"> </i>
+                    List of all categories
+                </div>
+                <div class="btn-actions-pane-right actions-icon-btn">
+                    <div class="btn-group dropdown"  style="margin-right: 10px">
+                        <a href="#">
+                            <button class="btn btn-success">
+                                <i class="fa fa-plus"></i> Add Category
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body" style="; border-top: 1px solid cadetblue; margin-top: 1px">
+                <table style="width: 100%; font-size: small" id="example" class="table table-hover table-striped table-bordered">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Category Name</th>
+                        <th>Description</th>
+                        <th>Created By</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>System Architect</td>
+                        <td>Edinburgh</td>
+                        <td>Dahabu Saidi</td>
+                        <td>
+                            <a class="#" href="#" style="text-decoration: none">
+                                <button class="btn btn-info btn-sm" title="CLick here to view more details">
+                                    <i class="fa fa-eye"></i>
+                                </button>
+                            </a>
+
+                            <a class="#" href="#" style="text-decoration: none">
+                                <button class="btn btn-success btn-sm" title="Click here to edit this category">
+                                    <i class="fa fa-edit"></i>
+                                </button>
+                            </a>
+
+                            <a class="#" href="#" style="text-decoration: none">
+                                <button class="btn btn-danger btn-sm" title="Click here to delete category">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+                            </a>
+                        </td>
+                    </tr>
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <th>#</th>
+                        <th>Category Name</th>
+                        <th>Description</th>
+                        <th>Created By</th>
+                        <th>Actions</th>
+                    </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
