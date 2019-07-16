397a5b3190b0d64daa7095518f560cd641d0ac06
dahabu saidi <dahabusaidi@gmail.com>
2019-07-16 23:10:21 +0300

    add rules to Job model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -12,4 +12,28 @@ class Job extends Model
     protected $keyType = 'string';
 
     public $incrementing = false;
+
+    protected $fillable = [
+        'category_name',
+        'description',
+        'added_by',
+    ];
+
+    public static function rules(){
+        return [
+            'title' => 'required',
+            'dead_line' => 'required',
+            'announce_date' => 'required',
+            'description' => 'required',
+            'organization' => 'required',
+            'job_link' => 'required',
+            'image_url' => 'required',
+            'position' => 'required',
+        ];
+
+    }
+
+    public function categories(){
+        return $this->belongsTo('App\Models\categories', 'category_id');
+    }
 }
