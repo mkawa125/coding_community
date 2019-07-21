d92a7091626c580daddcd44737e116a70db75418
dahabu saidi <dahabusaidi@gmail.com>
2019-07-21 13:51:40 +0300

    add subcategory field dynamically

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -79,5 +79,32 @@
             </div>
         </div>
     </div>
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+            var maxField = 10; //Input fields increment limitation
+            var addButton = $('.add_button'); //Add button selector
+            var wrapper = $('.field_wrapper'); //Input field wrapper
+            var fieldHTML = '<div class="input-group" style="margin-top: 8px"><input type="text" class="form-control" placeholder="New Subcategory" name="subcategories[]" value=""/>' +
+                '<a href="javascript:void(0);" style="border-radius: 0" class="remove_button btn btn-danger"><i class="fa fa-minus-circle"></i></a></div>'; //New input field html
+            var x = 1; //Initial field counter is 1
+
+            //Once add button is clicked
+            $(addButton).click(function(){
+                //Check maximum number of input fields
+                if(x < maxField){
+                    x++; //Increment field counter
+                    $(wrapper).append(fieldHTML); //Add field html
+                }
+            });
+
+            //Once remove button is clicked
+            $(wrapper).on('click', '.remove_button', function(e){
+                e.preventDefault();
+                $(this).parent('div').remove(); //Remove field html
+                x--; //Decrement field counter
+            });
+        });
+    </script>
 @endsection
 
