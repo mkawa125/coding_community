17fd671db8479c04dd497b9c32e84ea8db30f43f
dahabu saidi <dahabusaidi@gmail.com>
2019-07-21 19:23:05 +0300

    add notebook controller

---
+++ resources/views/notebook/index.blade.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mkawa
+ * Date: 7/21/19
+ * Time: 7:20 PM
+ */
+?>
+@extends('layouts.app')
+
+@section('content')
+    <div class="col-md-11" style="margin: 0 auto">
+        @if($message = Session::get('message'))
+            <div class="alert alert-success alert-block">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                {{ $message }}
+            </div>
+        @endif
+        <div class="card mb-3" style="padding: 15px">
+
+
+            <div class="card-header-tab card-header" style=" border-bottom: 1px solid cadetblue">
+                <div class="card-header-title font-size-lg text-capitalize font-weight-normal">
+                    <i class="header-icon fa fa-list mr-3 text-muted opacity-6" style="font-size: small"> </i>
+                    List of all categories
+                </div>
+                <div class="btn-actions-pane-right actions-icon-btn">
+                    <div class="btn-group dropdown"  style="margin-right: 10px">
+                        <a href="{{ route('categories.create') }}">
+                            <button class="btn btn-success">
+                                <i class="fa fa-plus"></i> Add Category
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body" style="; border-top: 1px solid cadetblue; margin-top: 1px">
+                Notes goes here
+            </div>
+        </div>
+    </div>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::middleware('auth')->group(function (){
     Route::resource('categories', 'CategoriesController');
     Route::resource('users', 'UsersController');
     Route::resource('jobs', 'JobsController');
+    Route::resource('myNotebook', 'MyStorageController');
 
 });
 //user notes routes
