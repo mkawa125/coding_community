84acbb3dd84302d2cff61f2701d597834b91498e
dahabu saidi <dahabusaidi@gmail.com>
2019-09-24 16:04:18 +0300

    add exception

--- tests/Feature/viewCategoryTest.php
+++ tests/Feature/viewCategoryTest.php
@@ -6,9 +6,13 @@ use App\Models\Category;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class viewCategoryTest extends TestCase
 {
+    use WithoutMiddleware;
+//    use RefreshDatabase;
+
     public function testGetAllCategories(){
         $category = factory(Category::class)->create();
         $resp = $this->get('/categories');
