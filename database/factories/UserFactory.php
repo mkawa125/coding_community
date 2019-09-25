4f2a173c187f9a63201ba152ca2a5dcd0edd1ce6
dahabu saidi <dahabusaidi@gmail.com>
2019-09-25 11:00:16 +0300

    add user to factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,11 +14,15 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
+    $gender = $faker->randomElement(['Male', 'Female']);
     return [
         'name' => $faker->name,
         'first_name' => $faker->name,
         'surname' => $faker->name,
         'username' => $faker->name,
+        'location' => $faker->address,
+        'phone_number' => $faker->phoneNumber,
+        'gender' => $gender,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
--- tests/Feature/viewCategoryTest.php
+++ tests/Feature/viewCategoryTest.php
@@ -7,15 +7,31 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+use App\Models\User;
 
 class viewCategoryTest extends TestCase
 {
     use WithoutMiddleware;
+    use WithFaker;
 //    use RefreshDatabase;
 
+    /** @test */
     public function testGetAllCategories(){
         $category = factory(Category::class)->create();
         $resp = $this->get('/categories');
         $resp->assertStatus(200);
+    }
+
+    /** @test */
+    public function create_return_validation_error(){
+//        $userIds = User::all()->pluck('id')->toArray();
+//        $data = [
+//            'category_name' => $this->faker->name,
+//            'description' => $this->faker->sentence,
+//            'added_by' => $this->faker->randomElement($userIds)
+//        ];
+//        $response = $this->post('categories', $data);
+//        $response->assertStatus(302);
+//        $response->assertSessionHasErrors($data);
     }
 }
