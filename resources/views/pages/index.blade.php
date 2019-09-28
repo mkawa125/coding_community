29c3d016175543b8214d642f07b3396a7437fd66
dahabu saidi <dahabusaidi@gmail.com>
2019-09-29 00:14:01 +0300

    add sample index landing page

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mkawa
+ * Date: 9/28/19
+ * Time: 10:56 PM
+ */
+?>
+@extends('layouts.app')
+@section('content')
+    <div class="col-md-10 offset-1" style="background-color: whitesmoke; border-radius: 4px">
+        <div class="row" style="margin-top: 10px; padding: 5px 5px 5px 20px">
+            <div class="col-sm-11" style="text-align: center;">
+                <h6>TO DO </h6>
+                <p>Add landing page</p>
+            </div>
+        </div>
+    </div>
+    @endsection
