282ab7677da8f34328a0a2a1c46a8e79a00fdc09
dahabu saidi <dahabusaidi@gmail.com>
2019-10-07 17:24:22 +0300

    add folder model content

--- app/Models/folder.php
+++ app/Models/folder.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Emadadly\LaravelUuid\Uuids;
 
 class folder extends Model
 {
-    //
+    use Uuids;
+
+    protected $fillable = [
+        'folder_name',
+        'folder_color',
+        'description',
+        'user_id',
+    ];
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    public static function rules(){
+        return [
+            'folder_name' => 'required',
+            'description' => 'required|min:10',
+            'folder_color' => 'required|min:10',
+        ];
+    }
 }
